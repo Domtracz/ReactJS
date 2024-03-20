f1fd5f8f2c180fbfdcc3f5f7e61c0b1b80d108ad
Dominik Tracz <domtracz@gmail.com>
2024-03-20 14:47:52 -0400

    styke react components external,modul,inline commit

--- website/my-react-web/src/App.jsx
+++ website/my-react-web/src/App.jsx
@@ -2,11 +2,14 @@
 //import Footer from "./Footer";
 //import Food from "./Food";
 
-import Card from "./Card";
+//import Card from "./Card";
 
+import Button from "./Button";
 function App() {
   return(
-    <Card></Card>
+    <>
+    <Button/>
+    </>
   );
 }
 
