ed3bad1a35244f1e867eea09dd5154941af2ce95
Dominik Tracz <domtracz@gmail.com>
2024-03-26 11:37:37 -0400

    update objects commit

--- website/my-react-web/src/App.jsx
+++ website/my-react-web/src/App.jsx
@@ -1,32 +1,11 @@
-//import Header from "./Header"
-//import Footer from "./Footer";
-//import Food from "./Food";
 
-
-//import Button from "./Button";
-
-//import Card from "./Card";
-
-//import Button from "./Button";
-
-//import Student from "./Student";
-
-//import UserGreeting from "./UserGreeting";
-
-//import List from "./List";
-
-//import ProfilePicture from "./ProfilePicture";
-
-//import MyComponent from "./MyComponent";
-//import Counter from "./Counter.jsx";
-
-import ColorPicker from "./ColorPicker";
+import MyComponentUF from "./MyComponentUF";
 
 function App() {
   
   return(
     
-    <ColorPicker/>
+    <MyComponentUF/>
   );
 }
 
--- website/my-react-web/src/MyComponentUF.jsx
+++ website/my-react-web/src/MyComponentUF.jsx
@@ -11,7 +11,7 @@ import React,{useState} from "react"
 
 function MyComponentUF(){
 
-  const[count,setCount] = useState(0);
+  /*const[count,setCount] = useState(0);
   
   function increment(){
     setCount(c=>c+1);
@@ -24,8 +24,29 @@ function MyComponentUF(){
   function reset(){
     setCount(0);
   }
+  */
+const [car, setCar] =useState({year: 2014, 
+                              make: "Volkswagon", 
+                              model: "Passat",});
 
-return(<></>)
+function handleYearChange(event){
+setCar(c =>({...c, year:event.target.value}));
+}
+
+function handleMakeChange(event){
+  setCar(c =>({...c, make:event.target.value}));
+}
+
+function handleModelChange(event){
+  setCar(c =>({...c, model:event.target.value}));
+}
+return(<div>
+        <p>Your favorite car is: {car.year} {car.make} {car.model}</p>
+
+        <input type="number" value={car.year} onChange={handleYearChange}/> <br/>
+        <input type="text" value={car.make} onChange={handleMakeChange}/> <br/>
+        <input type="text" value={car.model} on onChange={handleModelChange}/> <br/>
+       </div>)
 }
 
 export default MyComponentUF
